a6a0a9cb993e91e1c3fbf887b9074ec9578180dc
Kerolos163 <kokoessa951@gmail.com>
2023-09-30 14:03:29 +0300

    first push

---
+++ lib/service/Weather_Service.dart
@@ -0,0 +1,26 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:weather_app/models/weathermodel/weathermodel.dart';
+
+class WeatherService {
+  final Dio dio;
+  final String baseURL = "http://api.weatherapi.com/v1/";
+  final String apiKey = "d499147de6564585b1f05804230507";
+  
+  WeatherService({required this.dio});
+  Future<Weathermodel> getWeather({required String cityName}) async {
+    try {
+      var response = await dio
+          .get("${baseURL}forecast.json?key=$apiKey&q=$cityName&days=1");
+      return Weathermodel.fromJson(response.data);
+    } on DioException catch (e) {
+      final String errorMessage = e.response?.data["error"]["message"] ??
+          "Oops there was an error, try later";
+      throw Exception(errorMessage);
+    } catch (e) {
+      log(e.toString());
+      throw Exception("Oops there was an error, try later");
+    }
+  }
+}
---
+++ lib/views/search_view.dart
@@ -0,0 +1,41 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:weather_app/models/weathermodel/weathermodel.dart';
+import 'package:weather_app/service/Weather_Service.dart';
+
+class SearchViewScreen extends StatelessWidget {
+  const SearchViewScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Search City"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              onSubmitted: (value) async {
+                Weathermodel model = await WeatherService(dio: Dio())
+                    .getWeather(cityName: value);
+                log(model.location!.name!);
+              },
+              decoration: const InputDecoration(
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+                label: Text("Search"),
+                hintText: "Enter City Name..",
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
